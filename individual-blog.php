d82a82c62648c470fe447a13b5fc10a4fe070ae5
Dilip-Bist <dilipbist879@gmail.com>
2025-08-07 16:14:34 +0545

    individual bolg page completed

--- individual-blog.php
+++ individual-blog.php
@@ -21,10 +21,202 @@ include 'inc/header.php';
 <!-- INDIVIDUAL BLOG SECTION STARTS  -->
 <section class="individual_blog_section p64">
     <div class="container">
-        <div class="row g-4">
+        <div class="row g-4 g-xxl-5">
             <div class="col-lg-8 order-2 order-lg-1">
                 <div class="individual_blog_left">
-                    
+                    <div class="individual_blog_large_img mb-5">
+                        <img src="assets/images/blog/blog3.jpg" alt="">
+
+                        <div class="tagline">
+                            <div class="cat">
+                                Lifesytle
+                            </div>
+                            <div class="d-flex align-items-center gap-2">
+                                <p>25 may 2025</p>
+                                <p>(03) comment</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <h2>male create fruit signs</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem culpa fugit officiis quia amet, distinctio quos delectus eaque nobis rem labore corrupti voluptatibus quod, porro aliquam natus tempora, fuga blanditiis dicta. Eveniet eum aperiam sit ex! Recusandae impedit voluptatem odit, quos officiis autem explicabo iusto harum dolore nostrum est quam aliquam sapiente exercitationem dolores tempora? Debitis, itaque nisi. Non, quis distinctio quibusdam repudiandae, ab quae perferendis eligendi rem a fuga asperiores accusantium dignissimos voluptatibus illo corrupti sint <a href="#">voluptatem</a> porro sapiente sequi error illum tenetur magnam assumenda voluptatum. Odio ipsa ab minima amet iusto provident tempore, nisi nemo possimus voluptates pariatur?</p>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ullam voluptas accusamus tempora vero fuga? Expedita, excepturi laborum maxime eligendi reiciendis iusto veniam at adipisci placeat sunt rem minus nobis, inventore harum numquam. Rerum perferendis similique, dolore consectetur repudiandae error vitae velit! Tempora <a href="#">necessitatibus</a> eveniet dolores hic ipsum corporis dolorum.</p>
+
+                    <div class="individual_blog_quote d-flex flex-column flex-md-row gap-md-3 mb-3">
+                        <div class="icon">
+                            <i class="ri-double-quotes-l"></i>
+                        </div>
+                        <div>
+                            <h4>Can their have divide good fourth hallon called one were better grass firmament moring after fishing likeness herbs green his.</h4>
+                            <p>BY: robinson crusd</p>
+                        </div>
+                    </div>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum non nam accusamus praesentium blanditiis nulla ea natus saepe debitis reprehenderit minima, facere nostrum! Libero hic doloribus quisquam, reiciendis distinctio magni deleniti facilis veniam, iusto obcaecati quibusdam. Officia placeat asperiores nemo iusto, magnam dolore illum dignissimos ea veritatis, debitis minima! Facilis nam, quod repudiandae earum mollitia perferendis similique sed magni facere!</p>
+
+                    <h3>Enhancing Your Inner Beauty </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate vitae nemo dolorem asperiores molestiae, repudiandae provident molestias maxime aut eos suscipit at rerum iusto impedit, modi accusantium maiores omnis repellat perspiciatis doloribus. Repudiandae voluptate reprehenderit quia fuga corrupti saepe soluta sint deserunt maiores voluptatum, asperiores aliquid. Deleniti ea praesentium mollitia cum, amet saepe doloremque laboriosam repellendus excepturi temporibus magnam tempore enim ut facilis distinctio quisquam est, quasi dolores aliquam eos eius blanditiis earum. Labore, sunt libero modi ab perferendis repellat tempore dolorum cupiditate veniam aperiam nisi delectus fugiat sequi, quod ullam neque iure. Eius tempore incidunt reiciendis. Rerum, fugit veniam.</p>
+
+                    <div class="inner_image">
+                        <div class="row g-3 my-4">
+                            <div class="col-lg-6">
+                                <img src="assets/images/blog/blog2.jpg" alt="image">
+                            </div>
+
+                            <div class="col-lg-6">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat reiciendis, recusandae repudiandae ipsam quasi velit aut nam facere dicta totam maxime doloribus earum officia inventore soluta doloremque quia perspiciatis assumenda dolorem molestiae numquam? Hic, dignissimos ipsa.</p>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cupiditate magnam optio voluptate repellat dolore repellendus in totam? Libero, reprehenderit aspernatur harum est provident velit nesciunt quidem dicta nisi doloribus incidunt sint voluptatem iusto culpa quasi nulla cumque earum, repudiandae odio dignissimos sed. Odio molestiae magni nulla iusto rerum ipsum dolorem excepturi quidem tempora, accusantium incidunt porro repellat voluptates in obcaecati magnam at dolorum maxime pariatur vero! Voluptatum aut quibusdam tempora.</p>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis aliquid omnis numquam quo. Incidunt ullam, eveniet sunt natus quam labore deleniti qui perferendis velit id minus quidem quo aliquam? Possimus.</p>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error sit dicta inventore impedit mollitia velit commodi, similique officiis ullam, vitae temporibus dolorum debitis aut, veniam a hic enim earum necessitatibus quae <a href="#">laudantium</a> unde adipisci alias ut perferendis? Nam et id perspiciatis labore est obcaecati nostrum aut cupiditate totam dolorum deleniti assumenda eius possimus molestias, error repellendus libero esse dolor alias distinctio dolore sunt quos eum saepe. Cum reiciendis consequuntur sequi.</p>
+
+                    <h3>Fashion is Nothing without People </h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem culpa fugit officiis quia amet, distinctio quos delectus eaque nobis rem labore corrupti voluptatibus quod, porro aliquam natus tempora, fuga blanditiis dicta. Eveniet eum aperiam sit ex! Recusandae impedit voluptatem odit, quos officiis autem explicabo iusto harum dolore nostrum est quam aliquam sapiente exercitationem dolores tempora? Debitis, itaque nisi. Non, quis distinctio quibusdam repudiandae, ab quae perferendis eligendi rem a fuga asperiores accusantium dignissimos voluptatibus illo corrupti sint <a href="#">voluptatem</a> porro sapiente sequi error illum tenetur magnam assumenda voluptatum. Odio ipsa ab minima amet iusto provident tempore, nisi nemo possimus voluptates pariatur?</p>
+
+
+                    <!-- VIDEO PALY SECTION STARTS  -->
+                    <section class="video_paly_section my-3">
+                        <div class="video_paly_bg">
+                            <a href="https://www.youtube.com/watch?v=Jm7Satp7Zqk&list=RDk3g_WjLCsXM&index=29&ab_channel=T-Series"
+                                class="video_play_btn">
+                                <div class="play_btn_design">
+                                    <i class="ri-play-large-fill"></i>
+                                </div>
+
+                                <div class="border_class"></div>
+                            </a>
+                        </div>
+                    </section>
+                    <!-- VIDEO PALY SECTION ENDS -->
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, ea. Quisquam beatae ipsa non optio animi, cumque nesciunt ullam cum voluptatum <a href="#">praesentium</a> iste dolorem quibusdam ab velit maxime quis veritatis pariatur autem numquam excepturi doloribus fuga? Debitis saepe a soluta. Nesciunt minus aspernatur odit fugit ipsum eaque minima, dolorum perferendis reiciendis nihil expedita aliquam quam esse velit dicta? Sed, reprehenderit?</p>
+
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam consectetur Lorem, ipsum dolor.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam consectetur Lorem, ipsum dolor.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam consectetur Lorem, ipsum dolor.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam consectetur Lorem, ipsum dolor.</li>
+                    </ul>
+
+
+
+
+                    <div class="indiv_product_tagline_container d-flex align-items-center justify-content-between gap-3 flex-wrap">
+                        <div class="d-flex align-items-center gap-1">
+                            <h5>Tags:</h5>
+                            <p>Fashion, Lifestyle, Model</p>
+                        </div>
+
+                        <div class="indi_social_links">
+                            <a href="#">
+                                <i class="ri-facebook-circle-fill"></i>
+                            </a>
+
+                            <a href="#">
+                                <i class="ri-instagram-fill"></i>
+                            </a>
+
+                            <a href="#">
+                                <i class="ri-twitter-x-fill"></i>
+                            </a>
+
+                            <a href="#">
+                                <i class="ri-linkedin-box-fill"></i>
+                            </a>
+
+                            <a href="#">
+                                <i class="ri-youtube-fill"></i>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="indi_author_container d-flex flex-column flex-md-row">
+                        <div class="aut_img">
+                            <img src="assets/images/client/client2.jpg" alt="client image">
+                        </div>
+                        <div class="aut_descripton">
+                            <h6>Author</h6>
+                            <h4>Ronald Fenoma</h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem accusantium adipisci dolorum dicta, reiciendis a fuga nostrum, voluptate rem dolore quaerat ipsum mollitia vel vitae excepturi sit doloribus? Libero, iste?</p>
+                        </div>
+                    </div>
+
+                    <div class="indi_blog_comment_container mt-5">
+                        <h3>Comments (2)</h3>
+
+                        <div class="comment_details mt-4 d-flex flex-column gap-4">
+                            <div class="d-flex flex-column flex-md-row align-items-md-center gap-3">
+                                <div class="commenter_img">
+                                    <img src="assets/images/client/client1.jpeg" alt="preson image">
+                                </div>
+                                <div class="comment_Detail_container">
+                                    <div class="d-flex align-items-center gap-3 flex-wrap justify-content-between">
+                                        <div class="commenter_comments">
+                                            <div class="name">
+                                                Lucy Steward
+                                            </div>
+                                            <div class="date">
+                                                08 sep 2025 at 9:30 pm
+                                            </div>
+                                        </div>
+                                        <button type="button">Replay <i class="ri-add-fill"></i></button>
+                                    </div>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, perspiciatis eum itaque repellat a doloremque fugit nemo dolor commodi similique.</p>
+                                </div>
+
+                            </div>
+                            <div class="d-flex flex-column flex-md-row align-items-md-center gap-3 ">
+                                <div class="commenter_img">
+                                    <img src="assets/images/client/client3.jpg" alt="preson image">
+                                </div>
+                                <div class="comment_Detail_container">
+                                    <div class="d-flex align-items-center gap-3 flex-wrap justify-content-between">
+                                        <div class="commenter_comments">
+                                            <div class="name">
+                                                Lucy Steward
+                                            </div>
+                                            <div class="date">
+                                                08 sep 2025 at 9:30 pm
+                                            </div>
+                                        </div>
+                                        <button type="button">Replay <i class="ri-add-fill"></i></button>
+                                    </div>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, perspiciatis eum itaque repellat a doloremque fugit nemo dolor commodi similique.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="comment_form mt-lg-5">
+                        <form action="">
+                            <h3>Leave A Replay</h3>
+
+                            <div class="row g-3">
+                                <div class="col-12">
+                                    <textarea name="comment" placeholder="Write Your Comment"></textarea>
+                                </div>
+                                <div class="col-md-6">
+                                    <input type="text" class="input" name="fullname" placeholder="Full Name">
+                                </div>
+                                <div class="col-md-6">
+                                    <input type="email" class="input" name="Email" placeholder="Your Email">
+                                </div>
+                                <div class="col-12">
+                                    <div class="d-flex align-items-center gap-2">
+                                        <input type="checkbox" name="chekbox" id="checkbox">
+                                        <label for="checkbox">Save my name, and webstite in this browser for the time i comment.</label>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <button>post comment</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
 
@@ -161,5 +353,48 @@ include 'inc/header.php';
 </section>
 <!--INDIVIDUAL BLOG SECTION ENDS -->
 
-<?php include 'inc/footer.php'; ?>
 
+<!-- MORE RELATED PRODUCTS SECTION  -->
+<section class="more_related_products p64">
+    <div class="container">
+        <div class="common_heading">
+            <h3>Related blog post</h3>
+        </div>
+
+        <div class="row g-4 mt-4">
+            <?php foreach (array_slice($blogposts,3,3) as $blog) : ?>
+                <div class="col-md-6 col-lg-4">
+                    <div class="blog_left_div">
+                        <div class="blog_info_container">
+                            <div class="blog_img">
+                                <img src="<?= $blog['image']; ?>" alt="blogimg">
+                            </div>
+
+                            <div class="blog_description">
+                                <div class="blog_tag_container">
+                                    <div class="blog_cat">
+                                        <?php echo $blog['category'] ?>
+                                    </div>
+                                    <div class="blog_date">
+                                        <?= $blog['date'] ?>
+                                    </div>
+                                    <div class="blog_comment">
+                                        (<?= $blog['comments'] ?>)
+                                        comment
+                                    </div>
+                                </div>
+
+                                <h3><?= $blog['title'] ?></h3>
+                                <p><?= $blog['description'] ?></p>
+                                <a href="individual-blog.php">continue Reading</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            <?php endforeach; ?>
+        </div>
+    </div>
+</section>
+<!-- MORE RELATED PRODUCTS SECTION ENDS -->
+<?php include 'inc/footer.php'; ?>
