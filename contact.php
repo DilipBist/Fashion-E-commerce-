c81bd8a3057b75548abc89b1b05e7807dc3cf2fc
Dilip-Bist <dilipbist879@gmail.com>
2025-08-07 16:16:43 +0545

    create the contact.php file

---
+++ contact.php
@@ -0,0 +1,22 @@
+<?php
+$pageTitle = 'Contact';
+include 'inc/header.php';
+?>
+
+
+
+<!-- breadcrumb section starts  -->
+<section class="bread_crumb_section">
+    <img src="./assets/images/backgrounds/crumpwave.jpg" alt="wave bg">
+    <div class="container position-absolute top-50 start-50 translate-middle">
+        <div class="bread_crump_link">
+            <a href="index.php">Home</a>
+            <span>/</span>
+            <p>Contact Us</p>
+        </div>
+    </div>
+</section>
+<!-- breadcrumb section ends  -->
+
+
+<?php include 'inc/footer.php'; ?>
